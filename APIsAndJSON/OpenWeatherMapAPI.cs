97195a7cf712277a8f24383b50daaf1e1d1fc943
Justin Simon <justinsimoncs@gmail.com>
2025-12-06 07:32:44 -0800

    Adding exercise 2

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        var tempApiCall = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={e1d441b29490684c15e5d6ff7039b9bc} 
+        //Use the api to get the current weather 
+
+
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -12,11 +12,40 @@ namespace APIsAndJSON
           var quote = new QuoteGenerator(client);
 
             //interate through 5 quotes from each API
-            for (int i = 0; i < 5; i++)
+            //Exercise 1 implementation
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine($"Kanye Quote {i + 1}: {quote.Kanye()}");
+            //    Console.WriteLine($"Ron Swanson Quote {i + 1}: {quote.RonSwanson()}");
+            //}
+
+
+
+
+
+            Console.WriteLine("Please enter in api key.");
+            var apiKey = Console.ReadLine();
+            while(true)
             {
-                Console.WriteLine($"Kanye Quote {i + 1}: {quote.Kanye()}");
-                Console.WriteLine($"Ron Swanson Quote {i + 1}: {quote.RonSwanson()}");
+                Console.WriteLine();
+                Console.WriteLine("Please enter in the city name");
+                var cityName = Console.ReadLine();
+                var weatherURL = "";
+
+                var response = client.GetStringAsync(weatherURL).Result;
+                //var formattedResponse = JValue.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(response);
+                Console.WriteLine();
+
             }
         }
     }
 }
+
+
+
+//Create an OpenWeatherMap account to get a free API Key
+//Add the NewtonSoft.Json Nuget Package to your Console App
+//Figure out how to get the CURRENT weather for the City you specify in degrees fahrenheit
+//Use this site for extra help: https://openweathermap.org/current  
+//Creatively display &organize the response
